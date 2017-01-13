ca3cca40cc5254f7ca3ed90abe3af002641602cc
Kyle Polich <kylepolich@gmail.com>
2017-01-13 18:42:17 +0000

    remove junk file

--- www/page-data.php
+++ www/page-data.php
@@ -1,5 +1,6 @@
 <?php
-  include('../../funcs.php');
+  include('../config.php');
+  $conn = mysqli_connect($host, $user, $password, "gsow");
   ini_set('display_errors',1);
   ini_set('display_startup_errors',1);
   error_reporting(-1);
